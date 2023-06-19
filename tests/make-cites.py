2c66edf2875cf9c1444497a67ab05fc7cb42712e
Zeping Lee <zepinglee@gmail.com>
2023-06-19 23:31:31 +0800

    法学引注手册：更新测试数据

---
+++ tests/make-cites.py
@@ -0,0 +1,32 @@
+import json
+import os
+import sys
+
+
+# python3 make-cites.py tests/styles/301manual-of-legal-citation-multi-lingual/test-data.json
+
+
+TEST_DIR = os.path.join('tests', 'styles')
+
+
+def main():
+    paths = []
+    if len(sys.argv) < 2:
+        raise ValueError
+
+    data_paths = sys.argv[1:]
+
+    for data_path in data_paths:
+        with open(data_path) as f:
+            data = json.load(f)
+        cites = [[{'id': item['id']}] for item in sorted(data, key=lambda x: x['id'])]
+
+        test_dir, data_file = os.path.split(data_path)
+        cites_path = os.path.join(test_dir, 'test-cites.json')
+        with open(cites_path, 'w') as f:
+            json.dump(cites, f, indent='\t', ensure_ascii=False)
+            f.write('\n')
+
+
+if __name__ == '__main__':
+    main()
